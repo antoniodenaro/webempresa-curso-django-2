d166fb1a37327681f52caa187abb640417bc1cf4
Antonio Denaro <antoniodenaro@outlook.com>
2021-07-09 18:02:15 -0300

    Añadido proyecto

---
+++ webempresa/services/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Service
+
+
+# Register your models here.
+class ServiceAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',  'updated')
+    list_display = ('title',)
+
+
+admin.site.register(Service, ServiceAdmin)
